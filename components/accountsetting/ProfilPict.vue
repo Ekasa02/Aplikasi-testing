6eeb35766596092d85680e9fc52e46621e641c49
Ekasa02 <safitrieka636@gmail.com>
2023-06-12 18:10:00 +0700

    update image

--- components/accountsetting/ProfilPict.vue
+++ components/accountsetting/ProfilPict.vue
@@ -5,19 +5,22 @@
         </div>
         <div class="flex items-center gap-x-12 mt-3">
             <div class="">
-                <img :src="items.img_url" class="w-[110px] h-[110px]  rounded-full" alt="Profile picture">
+                <img :src="getPhoto" class="w-[110px] h-[110px]  rounded-full" alt="Profile picture">
             </div>
             <div class="">
                 <p>Maximum photo size is 1 MB</p>
-                <button-select></button-select>
+                <button-select
+                @file="handleFile"/>
             </div>
         </div>
-        <div class="pt-6">
+        <div
+        v-if="items?.email"
+        class="pt-6">
             <label class="block font-['Montserrat'] font-bold text-[14px] mb-2" for="email-member">
               Email
             </label>
             <input
-              v-model="items.email"
+            v-model="email"
               id="email-member"
               class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="email"
@@ -26,13 +29,15 @@
 
             >
           </div>
-          <div class="pt-7">
+          <div 
+          v-if="items?.name"
+          class="pt-7">
             <label class="block font-['Montserrat'] font-bold text-[14px] mb-2" for="project-name">
               Name
             </label>
             
             <input
-            v-model="items.name"
+            v-model="name"
               id="name"
               class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
@@ -40,7 +45,8 @@
             >
           </div>
           <div class="md:pt-9 md:pb-10 py-6 w-full">
-            <button class="font-['Montserrat'] text-white bg-[#554AF0]  font-semibold py-2 px-5 rounded ">
+            <button class="font-['Montserrat'] text-white bg-[#554AF0]  font-semibold py-2 px-5 rounded "
+            @click="updateData">
                 Save
             </button>
         </div>
@@ -56,12 +62,21 @@ export default {
     },
     data(){
       return{
-        items:[],
-        img_data: []
+        items:null,
+        imageFile : null,
+        name : "",
+        preview : null,
+        email : ""
       }
     },
     mounted (){
       this.getProfile();
+    },
+    computed : {
+      getPhoto(){
+        if(!this.items) return ''
+       return this.preview ? this.preview : this.items.img_url
+      }
     },
     methods:{
       async getProfile() {
@@ -74,7 +89,42 @@ export default {
             } catch (e) {
                 console.log(e)
             }
+        },
+      handleFile(file){
+        this.preview = file.base64
+        this.imageFile = file.file
+      },
+      async updateData(){
+        try{
+          const formData = new FormData()
+          if(this.imageFile){
+            formData.append('img_url',this.imageFile)
+          }
+          if(this.name){
+            formData.append('name',this.name)
+          }
+
+          if(this.name){
+
+            const data = await this.$axios.$put('/profiles',formData,{
+              'content-type' : 'multipart/form-data'
+            })
+  
+            console.log(data)
+          }
         }
+        catch(e){
+          console.log(e.response)
+        }
+      }
+    },
+    watch : {
+      items(){
+        if(this.items){
+          this.name = this.items.name
+          this.email = this.items.email
+        }
+      }
     }
 }
 </script>
--- components/profil/ButtonSelect.vue
+++ components/profil/ButtonSelect.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="pt-5">
         <label for="input-file" class="cursor-pointer ">
-          <input id="input-file" type="file" accept="image/*" class="hidden" ref="file" @change="handleFileSelect"/>
+          <input 
+          id="input-file" 
+          type="file" accept="image/*" 
+          class="hidden" 
+          ref="file" 
+          @change="handleFileSelect"/>
             <button 
             @click="handleFile"
             class="font-['Montserrat'] text-white bg-[#554AF0]  font-semibold py-2 px-5 rounded ">
@@ -14,13 +19,34 @@
 <script>
 
 export default {
+    data(){
+        return {
+            preview :null
+        }
+    },
     methods : {
         handleFile(){
             this.$refs.file.click()
         },
         handleFileSelect(event){
-            const file = event.target.files[0];
-            this.$emit("file", file);
+
+            const reader = new FileReader()
+
+            if(event.target.files?.length > 0){
+                reader.onload = e => {
+                    this.preview = e.target.result
+                    this.$emit('file', 
+                    {
+                        file : event.target.files[0],
+                        base64 : this.preview
+                    })
+                }
+                reader.readAsDataURL(event.target.files[0])
+            }
+
+            // console.log(event)
+            // // const file = event.target.files[0];
+            // this.$emit("file", file);
         }
     }
 }
