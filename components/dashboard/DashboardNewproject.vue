a42a0656db52aaa7a4139ea8f454d34fc586eaa5
Ekasa02 <safitrieka636@gmail.com>
2023-06-16 20:06:51 +0700

    Automate

--- components/dashboard/DashboardNewproject.vue
+++ components/dashboard/DashboardNewproject.vue
@@ -75,7 +75,7 @@
             <select
               id="platform"
               v-model="newItem.platform"
-              
+              :disabled="isPlatformDisabled"
               required
               class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Platform"
